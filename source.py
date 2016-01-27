15a051adfb48eaef810bd15800d307790ce975b5
asdofe <asdofe@gmail.com>
2016-01-27 13:48:03 +0800

    Maximum Depth of Binary Tree
    https://leetcode.com/problems/maximum-depth-of-binary-tree/

--- source.py
+++ source.py
@@ -1,10 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
-    def singleNumber(self, nums):
+    def maxDepth(self, root):
         """
-        :type nums: List[int]
+        :type root: TreeNode
         :rtype: int
         """
-        # xor有交換律，把全部的數xor起來即為所求
-        return reduce(lambda a, b: a^b, nums)
-
+        # md(x) = 0 ,如果 x 是 空
+        #       = 1 ,如果 x 的 left和right都是空
+        #       = 1 + max(md(x.left), md(x.right)) ,如果不是上面兩個狀況 
+        md = lambda x: 0 if not x else 1 if x.left == None and x.right == None else 1 + max(md(x.left), md(x.right))
+        return md(root)
         
